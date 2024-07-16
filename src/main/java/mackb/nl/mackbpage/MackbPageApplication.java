92144b3faae170f513ad4daf3a17509f1b746983
MackBak <154362357+MackBak@users.noreply.github.com>
2024-07-16 15:39:02 +0200

    init

---
+++ src/main/java/mackb/nl/mackbpage/MackbPageApplication.java
@@ -0,0 +1,13 @@
+package mackb.nl.mackbpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MackbPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MackbPageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mackb/nl/mackbpage/MackbPageApplicationTests.java
@@ -0,0 +1,13 @@
+package mackb.nl.mackbpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MackbPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
