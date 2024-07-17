92217092cbce8e70a62d7c75e26c125f436b0246
MackBak <154362357+MackBak@users.noreply.github.com>
2024-07-17 17:09:55 +0200

    test

---
+++ src/main/java/mackb/nl/mackbpage/business/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package mackb.nl.mackbpage.business.service;
+
+import mackb.nl.mackbpage.business.model.User;
+import mackb.nl.mackbpage.persistence.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        UserBuilder builder = org.springframework.security.core.userdetails.User.withUsername(username);
+        builder.password(user.getPassword());
+        builder.roles("USER");
+        return builder.build();
+    }
+}
