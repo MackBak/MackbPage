7a391ae53cc07f5cd4bd7ce48514e994c121a96a
MackBak <154362357+MackBak@users.noreply.github.com>
2024-07-17 14:02:03 +0200

    fix: iratingcheckerpage visible

--- src/main/java/mackb/nl/mackbpage/communication/IRatingCheckerController.java
+++ src/main/java/mackb/nl/mackbpage/communication/IRatingCheckerController.java
@@ -2,11 +2,13 @@ package mackb.nl.mackbpage.communication;
 
 import mackb.nl.mackbpage.business.model.Pilot;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
@@ -16,6 +18,11 @@ import java.util.stream.Collectors;
 @Controller
 @RequestMapping("/irating")
 public class IRatingCheckerController {
+
+    @GetMapping("/iRatingChecker")
+    public String showIRatingCheckerPage() {
+        return "iRatingChecker"; // This corresponds to the iRatingChecker.html template
+    }
 
     @PostMapping("/upload")
     public ModelAndView handleFileUpload(@RequestParam("csvFile") MultipartFile csvFile,
